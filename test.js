a73a5c37976b58fbc686b606119227ced756e8bc
Ole-Martin Mørk <olemartin@gmail.com>
2017-08-23 10:58:20 +0200

    Lagt til en test som henter neste buss fra Lundliveien-stoppet

---
+++ test.js
@@ -0,0 +1,15 @@
+const ruter = require('ruter-api');
+const utm = require('utm')
+var dateformat = require('dateformatter').format;
+
+const location = utm.fromLatLon(59.9382647,10.8129383, 32);
+ruter.api("Place/GetClosestStops?coordinates=(x="+Math.round(location.easting)+",y="+Math.round(location.northing)+")", {}, response => {
+    result = response;
+    ruter.api("StopVisit/GetDepartures/"+response.result[0].ID, {}, response => {
+        const expectedDepartureTime = response.result[0].MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime;
+        const date = new Date(expectedDepartureTime);
+        const name = response.result[0].MonitoredVehicleJourney.MonitoredCall.DestinationDisplay;
+
+        console.log("The bus " + name + " is leaving at " + dateformat("H:i:s", date)+ ". It's in " + parseInt(dateformat("i", date - new Date())) + " minutes");
+    });
+});
