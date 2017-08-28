5572fd613abd1f2518c7bc622ba4abd3ae685a6e
Ole-Martin Mørk <olemartin@gmail.com>
2017-08-28 21:47:54 +0200

    testing a service

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
+debug.log = console.log.bind(console);
+
 
 app.use(bodyParser.json());
 process.env.DEBUG = 'actions-on-google:* node index.js'
